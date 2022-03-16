0fe0d1e888721c724553af7678b1d8fd47bc064c
Dilip Agheda <dilip.agheda@origin.com.au>
2022-03-16 19:47:02 +1100

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Dilip Agheda on 16/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
